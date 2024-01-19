c5c73d6ccb0f8e7ed74045b2125a5637f92daa92
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-01-19 22:12:55 +0500

    Adding deep.py in deep directory of Week 1

---
+++ Week 1/deep/deep.py
@@ -0,0 +1,6 @@
+question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
+
+if question == "42" or question == "forty-two" or  question == "forty Two":
+    print("Yes")
+else:
+    print("No")
