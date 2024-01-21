00076f407718b4e8f051b9df379f8bf64e7eef8a
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-01-21 15:13:38 +0500

    Adding plates.py in plates directory of Week 2

---
+++ Week 2/plates/plates.py
@@ -0,0 +1,32 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if 2 <= len(s) <= 6:
+        return False
+
+    if s[:2].isalpha() == False:
+        return False
+
+    i = 0
+    while i < len(s):
+        if s[i].isalpha() == False:
+            if s[i] == "0":
+                return False
+            else:
+                break
+        i += 1
+
+    for c in s:
+        if c in [".", " ", "?", "!"]:
+            return False
+
+    return True
+
+
+main()
