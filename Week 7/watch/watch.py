190423fd7823361d4c66e905ce7154b3a3f031dc
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-01-25 18:29:22 +0500

    Adding watch.py in watch directory of Week 7

---
+++ Week 7/watch/watch.py
@@ -0,0 +1,18 @@
+import re
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    if re.search(r"<iframe(.)*><\/iframe>", s):
+        if url_pattern := re.search(
+            r"(https?)*:\/\/(www\.)?(youtube\.com)\/embed\/([a-zA-Z0-9]+)", s
+        ):
+            split_url = url_pattern.groups()
+            return "https://youtu.be/" + split_url[3]
+
+
+if __name__ == "__main__":
+    main()
