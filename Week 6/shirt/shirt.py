980a1d70c2dafb2a994704d58c3f4021af07616d
Muhammad Hanzala <139219974+mhanzalayousaf@users.noreply.github.com>
2024-07-01 18:39:51 +0500

    Update shirt.py

--- Week 6/shirt/shirt.py
+++ Week 6/shirt/shirt.py
@@ -1,27 +1,43 @@
-from PIL import Image, ImageOps
 import sys
+
+from PIL import Image, ImageOps
 
 
 def main():
     check_command_line_arg()
 
     try:
-        imagefile = Image.open(sys.argv[1])
-    except FileNotFoundError:
-        sys.exit("Input does not exists.")
+        image = Image.open(sys.argv[1])
+        shirt = Image.open("shirt.png")
+        new_image = ImageOps.fit(image, shirt.size)
+        new_image.paste(shirt, mask=shirt)
+        new_image.save(sys.argv[2])
 
-    shirtfile = Image.open("shirt.png")
-    size = shirtfile.size
-    muppet = ImageOps.fit(imagefile, size)
-    muppet.paste(shirtfile, shirtfile)
-    muppet.save(sys.argv[2])
+    except FileNotFoundError:
+        sys.exit(f"Could not find {sys.argv[1]}")
 
 
 def check_command_line_arg():
+    extensions = ['jpg', 'jpeg', 'png']
+
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
+    try:
+        root_1, extension_1 = sys.argv[1].split(".")
+        root_2, extension_2 = sys.argv[2].split(".")
+
+        if extension_1 != extension_2:
+            sys.exit("Input and output have different extentions")
+        if not extension_1 in extensions and not extension_2 in extensions:
+            sys.exit("Invalid input")
+    except ValueError:
+        sys.exit("Invalid input")
+
+
+if __name__ == "__main__":
+    main()
 
     types = (".jpg", ".jpeg", ".png")
     if not sys.argv[1].endswith(types) or not sys.argv[2].endswith(types):
